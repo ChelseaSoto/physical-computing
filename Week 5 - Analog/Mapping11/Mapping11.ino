e010878b0e6f3ffb5f4015dbf1fdac9079f821a7
Chelsea <ch951840@ucf.edu>
2024-09-25 14:06:39 -0400

    Update Mapping11.ino

--- Week 5 - Analog/Mapping11/Mapping11.ino
+++ Week 5 - Analog/Mapping11/Mapping11.ino
@@ -18,189 +18,128 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(digitalRead(CPLAY_SLIDESWITCHPIN)){
+  if (digitalRead(CPLAY_SLIDESWITCHPIN)) {
     int points = 0;
 
     //while loop to keep the player going until they win five times in a row
-    while(points < 5){
-    //initialize x and y axis variables
-    float xValue = CircuitPlayground.motionX();
-    float yValue = CircuitPlayground.motionY();
-    
-    //random number between 0-3
-    int says = random(0,4);
-    //switch statement  
-    switch(says){
-      case 0: //left +y
-        //lights up the left half of the board
-        CircuitPlayground.setPixelColor(7, 255, 255, 255);//#, r, g, b
-        CircuitPlayground.setPixelColor(8, 255, 255, 255);
-        CircuitPlayground.setPixelColor(9, 255, 255, 255);
-        CircuitPlayground.setPixelColor(0, 255, 255, 255);
-        CircuitPlayground.setPixelColor(1, 255, 255, 255);
-        CircuitPlayground.setPixelColor(2, 255, 255, 255);
-        delay(debounce);
-        
-        //checks if the player rotated it in the correct direction
-        points = positiveCheck(yValue, points);
-        break;
-      case 1: //right -y
-        //lights up the right half of the board
-        CircuitPlayground.setPixelColor(7, 255, 255, 255);//#, r, g, b
-        CircuitPlayground.setPixelColor(6, 255, 255, 255);
-        CircuitPlayground.setPixelColor(5, 255, 255, 255);
-        CircuitPlayground.setPixelColor(4, 255, 255, 255);
-        CircuitPlayground.setPixelColor(3, 255, 255, 255);
-        CircuitPlayground.setPixelColor(2, 255, 255, 255);
-        delay(debounce);
-        
-        //checks if the player rotated it in the correct direction
-        if (yValue < 0){ //yes
-          //flash green
-          for(int i = 0; i <= 9; i++){
-            CircuitPlayground.setPixelColor(i, 0, 255, 0);
-          }
-          
-          delay(debounce);
+    while (points < 5) {
+      //initialize x and y axis variables
+      float xValue = CircuitPlayground.motionX();
+      float yValue = CircuitPlayground.motionY();
 
-          CircuitPlayground.clearPixels();
-          points++;
-        }else{
-          //flash red
-          for(int i = 0; i <= 9; i++){
-            CircuitPlayground.setPixelColor(i, 255, 0, 0);
-          }
-          
+      //random number between 0-3
+      int says = random(0, 4);
+      //switch statement
+      switch (says) {
+        case 0:  //left +y
+          //lights up the left half of the board
+          simonSays(0, 2);
+          simonSays(7, 9);
+
           delay(debounce);
           CircuitPlayground.clearPixels();
-          //flash a second time
-          for(int i = 0; i <= 9; i++){
-            CircuitPlayground.setPixelColor(i, 255, 0, 0);
-          }
-          CircuitPlayground.clearPixels();
-          points = 0;
-        }
-        break;
-      case 2: //up +x
-        //lights up the upper half of the board
-        CircuitPlayground.setPixelColor(9, 255, 255, 255);//#, r, g, b
-        CircuitPlayground.setPixelColor(8, 255, 255, 255);
-        CircuitPlayground.setPixelColor(7, 255, 255, 255);
-        CircuitPlayground.setPixelColor(6, 255, 255, 255);
-        CircuitPlayground.setPixelColor(5, 255, 255, 255);
-        delay(debounce);
-        
-        //checks if the player rotated it in the correct direction
-        points = positiveCheck(yValue, points);
-        break;
-      case 3: //down -x
-        //lights up the upper half of the board
-        CircuitPlayground.setPixelColor(4, 255, 255, 255);//#, r, g, b
-        CircuitPlayground.setPixelColor(3, 255, 255, 255);
-        CircuitPlayground.setPixelColor(2, 255, 255, 255);
-        CircuitPlayground.setPixelColor(1, 255, 255, 255);
-        CircuitPlayground.setPixelColor(0, 255, 255, 255);
-        
-        delay(debounce);
-        CircuitPlayground.clearPixels();
-        
-        //checks if the player rotated it in the correct direction
-        if (xValue < 0){ //yes
-          //flash green
-          for(int i = 0; i <= 9; i++){
-            CircuitPlayground.setPixelColor(i, 0, 255, 0);
-          }
-          
-          delay(debounce);
 
-          CircuitPlayground.clearPixels();
-          points++;
-        }else{
-          //flash red
-          for(int i = 0; i <= 9; i++){
-            CircuitPlayground.setPixelColor(i, 255, 0, 0);
-          }
-          
+          //checks if the player rotated it in the correct direction
+          points = positiveCheck(yValue, points);
+          break;
+        case 1:  //right -y
+          //lights up the right half of the board
+          simonSays(2, 7);
           delay(debounce);
           CircuitPlayground.clearPixels();
-          //flash a second time
-          for(int i = 0; i <= 9; i++){
-            CircuitPlayground.setPixelColor(i, 255, 0, 0);
-          }
+
+          //checks if the player rotated it in the correct direction
+          points = negativeCheck(yValue, points);
+          break;
+        case 2:  //up +x
+          //lights up the upper half of the board
+          simonSays(5, 9);
+          delay(debounce);
           CircuitPlayground.clearPixels();
-          points = 0;
-        }
 
+          //checks if the player rotated it in the correct direction
+          points = positiveCheck(yValue, points);
+          break;
+        case 3:  //down -x
+          //lights up the upper half of the board
+          simonSays(0, 4);
+          delay(debounce);
+          CircuitPlayground.clearPixels();
 
-        break;
+          //checks if the player rotated it in the correct direction
+          points = negativeCheck(xValue, points);
+
+          break;
+      }
     }
+
+
+    //if win condition is met
+    if (points >= 5) {
+      //flash green three times
+      for (int i; i <= 3; i++) {
+        green();
+      }
+
+      //reset points to start the game again
+      points = 0;
     }
   }
 }
 
-int positiveCheck(float value, int points){
-    if (value > 0){ //yes
-          //flash green
-          for(int i = 0; i <= 9; i++){
-            CircuitPlayground.setPixelColor(i, 0, 255, 0);
-          }
-          
-          delay(debounce);
-
-          CircuitPlayground.clearPixels();
-          points++;
-        }else{
-          //flash red
-          for(int i = 0; i <= 9; i++){
-            CircuitPlayground.setPixelColor(i, 255, 0, 0);
-          }
-          
-          delay(debounce);
-          CircuitPlayground.clearPixels();
-          //flash a second time
-          for(int i = 0; i <= 9; i++){
-            CircuitPlayground.setPixelColor(i, 255, 0, 0);
-          }
-          CircuitPlayground.clearPixels();
-          points = 0;
-        }
-    return points;
+//Simon Says to . . .
+void simonSays(int min, int max) {
+  for (int i = min; i <= max; i++) {
+    CircuitPlayground.setPixelColor(i, 255, 255, 255);  //#, r, g, b
   }
-
-
-/*
-toggle switch turn on or off
-when turned on, animation maybe before
-
-int points = 0;
-while(points < 5){
-randomize numbers 
-0 left
-1 right
-2 up
-3 down
--
-4 block light
-5 sound
-
-
-switch statement
-case 0:
-  light up left half
-  check if player tilts left
-  if yes -> flash green or continue next, points++
-  if no -> flash red twice, points = 0
-  break; //will this break the switch statement only or the while loop too?
-case 1: 
-  light up right half
-  check if player tilts right
-  if yes -> flash green or continue next, points++
-  if no -> flash red twice, points = 0
-  break;
-.
-.
-.
 }
 
-once points == 5 -> flash green twice or something
-*/
+//checks if in the positive of the axis past the threshold
+int positiveCheck(float value, int points) {
+  if (value > threshold) {  //yes
+    //flash green once
+    green();
+    points++;
+  } else {  //no
+    //flash red twice
+    for (int i = 0; i <= 2; i++) {
+      red();
+    }
+    points = 0;
+  }
+  return points;
+}
+
+//checks if in the negative of the axis past the threshold
+int negativeCheck(float value, int points) {
+  if (value < threshold) {  //yes
+    //flash green once
+    green();
+    points++;
+  } else {  //no
+    //flash red twice
+    for (int i = 0; i <= 2; i++) {
+      red();
+    }
+    points = 0;
+  }
+  return points;
+}
+
+//flashes red once
+void red() {
+  for (int i = 0; i <= 9; i++) {
+    CircuitPlayground.setPixelColor(i, 255, 0, 0);  //#, r, g, b
+  }
+  delay(debounce);
+  CircuitPlayground.clearPixels();
+}
+
+//flashes green once
+void green() {
+  for (int i = 0; i <= 9; i++) {
+    CircuitPlayground.setPixelColor(i, 0, 255, 0);  //#, r, g, b
+  }
+  delay(debounce);
+  CircuitPlayground.clearPixels();
+}
